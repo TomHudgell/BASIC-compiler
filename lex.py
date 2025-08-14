c11b870854befb18c7950f35e84ba4330450193a
TomHudgell <thomas.hudgell@gmail.com>
2025-08-14 19:09:40 +0100

    Tokenising single chars

--- lex.py
+++ lex.py
@@ -1,3 +1,6 @@
+import enum
+import sys
+
 class Lexer:
     def __init__(self, source):
         self.source = source + '\n'  # source code to lex as a string, newline added to simplify processing of last statement
@@ -21,7 +24,7 @@ class Lexer:
 
     # invalid token found, print error message and exit
     def abort(self, message):
-        pass
+        sys.exit("Lexing error. " + message)
 
     # skip whitespaces excpet newlines, which we will use to indicate the end of a statement
     def skipWhitespace(self):
@@ -33,4 +36,65 @@ class Lexer:
 
     # return the next token
     def getToken(self):
-        pass
+        token = None
+
+        # check the first character of this toke to see if we can decide what it is
+        # if it is a multiple character token then we will continue processing
+        if self.curChar == '+':
+            token = Token(self.curChar, TokenType.PLUS)
+        elif self.curChar == '-':
+            token = Token(self.curChar, TokenType.MINUS)
+        elif self.curChar == '*':
+            token = Token(self.curChar, TokenType.ASTERISK)
+        elif self.curChar == '/':
+            token = Token(self.curChar, TokenType.SLASH)
+        elif self.curChar == '\n':
+            token = Token(self.curChar, TokenType.NEWLINE)
+        elif self.curChar == '\0':
+            token = Token('', TokenType.EOF)
+        else:
+            self.abort("Unknown token: " + self.curChar)  # unknown token
+
+        self.nextChar()
+        return token
+
+
+# token contains the original text and the type of token
+class Token:
+    def __init__(self, tokenText, tokenKind):
+        self.text = tokenText  # used for identifies, strings, numbers
+        self.kind = tokenKind
+
+# enum for all the types of tokens
+class TokenType(enum.Enum):
+    EOF = -1
+    NEWLINE = 0
+    NUMBER = 1
+    IDENT = 2
+    STRING = 3
+
+    # keywords
+    LABEL = 101
+    GOTO = 102
+    PRINT = 103
+    INPUT = 104
+    LET = 105
+    IF = 106
+    THEN = 107
+    ENDIF = 108
+    WHILE = 109
+    REPEAT = 110
+    ENDWHILE = 111
+
+    # operators
+    EQ = 201
+    PLUS = 202
+    MINUS = 203
+    ASTERISK = 204
+    SLASH = 205
+    EQEQ = 206
+    NOTEQ = 207
+    LT = 208
+    LTEQ = 209
+    GT = 210
+    GTEQ = 211
